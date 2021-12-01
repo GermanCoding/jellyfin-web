0aaec7077b5f48c5471e16a14f584f9b855fd0b6
GermanCoding <github@germancoding.com>
2021-12-01 21:52:42 +0100

    Direct link to the password reset server
    Jellyfin inbuild won't work anyway.

--- src/controllers/session/login/index.js
+++ src/controllers/session/login/index.js
@@ -243,7 +243,8 @@ export default function (view, params) {
         return false;
     });
     view.querySelector('.btnForgotPassword').addEventListener('click', function () {
-        Dashboard.navigate('forgotpassword.html');
+        // Dashboard.navigate('forgotpassword.html');
+        window.open("https://accounts.germancoding.com/resetpassword");
     });
     view.querySelector('.btnCancel').addEventListener('click', showVisualForm);
     view.querySelector('.btnQuick').addEventListener('click', function () {
